847b0bbbb613ae3a83757217be56bce4ee5e639e
eraldo6 <eraldograbovaj06@gmail.com>
2024-11-09 22:58:36 +0100

    adjusted scale

--- tools/common/env.sh
+++ tools/common/env.sh
@@ -7,7 +7,7 @@ export HADOOP_CLASSPATH=`hadoop classpath`
 export FLINK_HOME=${INSTALL_PATH}/packages/flink-1.16.0
 export FLINK_TEST_JAR=${INSTALL_PATH}/flink-tpcds/target/flink-tpcds-0.1-SNAPSHOT-jar-with-dependencies.jar
 
-export SCALE=10000
+export SCALE=100
 export FLINK_TEST_DB=tpcds_bin_orc_$SCALE
 
 # If you try to run TPC-DS tests on partition table, you need to use the below environment variables.
